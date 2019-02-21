135cb185c1ae017ade7205453e313fbdc7f0f4f1
Joel Margolis <joel@teknogeek.io>
2019-02-20 19:06:41 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# get_schemas
+
+Pretty straightforward, just gets all the URL schemas and the corresponding activities and prints them all out.
+
+You're going to want to decompile the app with `apktool` before running this so that you have a decoded copy of:
+
+- AndroidManifest.xml
+- res/values/strings.xml
+
+Both of these files are required for this tool to work as it substitutes any `@string/x_y_z` values.
+
+## Usage
+
+```
+$ ./get_schemas.py -h
+usage: get_schemas.py [-h] [-m MANIFEST] [-s STRINGS]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -m MANIFEST, --manifest MANIFEST
+                        Path to AndroidManifest.xml (default:
+                        ./AndroidManifest.xml)
+  -s STRINGS, --strings STRINGS
+                        Path to strings.xml (default:
+                        ./res/values/strings.xml)
+```
+
+## Example
+
+```
+$ ./get_schemas.py -m ./com.twitter.android/AndroidManifest.xml -s ./com.twitter.android/res/values/strings.xml
+com.twitter.android.ProfileActivity
+  content://com.android.contacts
+com.twitter.app.deeplink.UrlInterpreterActivity
+  http://mobile.twitter.com/.*
+  http://twitter.com/.*
+  http://www.twitter.com/.*
+  https://ads.twitter.com/mobile
+  https://mobile.twitter.com/.*
+  https://twitter.com/.*
+  https://www.twitter.com/.*
+  twitter://account
+  twitter://alerts/landing_page/.*
+  twitter://bouncer
+  twitter://broadcasts
+  twitter://collection
+  twitter://connect_people
+  twitter://dm_conversation
+  twitter://events/timeline/.*
+  twitter://events/tv_show/.*
+  twitter://explore
+  twitter://flow/setup_profile
+  twitter://follow
+  twitter://followers/verified
+  twitter://front
+  twitter://golive
+  twitter://hashtag/.*
+  twitter://intent/favorite
+  twitter://intent/follow
+  twitter://intent/like
+  twitter://intent/retweet
+  twitter://interest_picker
+  twitter://internal/.*
+  twitter://list
+  twitter://lists
+  twitter://live/timeline/.*
+  twitter://login
+  twitter://login-token
+  twitter://mentions
+  twitter://messages
+  twitter://moment
+  twitter://moments
+  twitter://moments/guide
+  twitter://moments/list
+  twitter://moments/maker
+  twitter://news
+  twitter://photo
+  twitter://post
+  twitter://profiles/edit
+  twitter://quote
+  twitter://search
+  twitter://session/new
+  twitter://settings
+  twitter://share_via_dm
+  twitter://signup
+  twitter://status
+  twitter://stickers
+  twitter://storystream
+  twitter://switch_to_logged_in_account
+  twitter://teams_invitation
+  twitter://timeline
+  twitter://topics
+  twitter://trends
+  twitter://tweet
+  twitter://tweet_previews
+  twitter://unfollow
+  twitter://user
+```
---
+++ get_schemas.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import argparse
+from bs4 import BeautifulSoup
+import itertools
+import re
+import sys
+
+is_scheme_data_tag = lambda tag: tag.name == 'data' and \
+    any(f'android:{x}' in tag.attrs for x in \
+        ('scheme', 'host', 'port', 'path', 'pathPrefix', 'pathPattern') \
+    )
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--manifest', 
+                        required=False,
+                        default='AndroidManifest.xml',
+                        type=argparse.FileType('r'),
+                        help='Path to AndroidManifest.xml (default: ./AndroidManifest.xml)')
+    parser.add_argument('-s', '--strings',
+                        required=False,
+                        default='res/values/strings.xml',
+                        type=argparse.FileType('r'), 
+                        help='Path to strings.xml (default: ./res/values/strings.xml)')
+    args = parser.parse_args()
+
+    strings_xml = BeautifulSoup(args.strings, 'xml')
+    strings = {d['name']: d.text for d in strings_xml.find_all('string', {'name': True})}
+
+    raw_manifest = args.manifest.read()
+    raw_manifest = re.sub('"@string\/(?P<string_name>[^"]+)"', lambda g: '"{}"'.format(strings.get(g.group('string_name'), 'UNKNOWN_STRING')), raw_manifest)
+    manifest_xml = BeautifulSoup(raw_manifest, 'xml')
+
+    activity_handlers = {}
+    for intent_filter in manifest_xml.find_all('intent-filter'):
+        scheme_items = intent_filter.findAll(is_scheme_data_tag)
+        if len(scheme_items) > 0:
+            activity_name = None
+            
+            # find activity name from parent
+            parent_elem = intent_filter.find_parent(['activity', 'activity-alias', 'service', 'receiver'])
+            if parent_elem:
+                # parent type
+                p_type = parent_elem.name
+                if p_type in ['activity', 'service', 'receiver']:
+                    activity_name = parent_elem['android:name']
+                elif p_type == 'activity-alias':
+                    target_activity_name = parent_elem['android:targetActivity']
+                    target_activity = manifest_xml.find('activity', {'android:name': target_activity_name})
+                    if target_activity:
+                        activity_name = target_activity['android:name']
+
+            if activity_name is not None:
+                schemes, hosts, ports, paths = [], [], [], []
+                for item in scheme_items:
+                    schemes.append(item.get('android:scheme'))
+                    hosts.append(item.get('android:host'))
+                    ports.append(item.get('android:port'))
+                    
+                    for k in ('path', 'pathPrefix', 'pathPattern'):
+                        paths.append(item.get(f'android:{k}'))
+
+                schemes, hosts, ports, paths = map(list, map(set, map(lambda x: filter(None, x), [schemes, hosts, ports, paths])))
+                no_port_path = (len(ports) == 0 and len(paths) == 0)
+                if len(ports) == 0: ports.append('')
+                if len(paths) == 0: paths.append('')
+                if len(hosts) == 0: hosts.append('') # for filters with only <schema>://
+                for scheme, host, port, path in list(itertools.product(schemes, hosts, ports, paths)):
+                    if scheme:
+                        uri = f'{scheme}://'
+                        if host:
+                            uri += host
+                            if not no_port_path:
+                                if port: uri += f':{port}'
+                                if path: uri += f'{"/" if not path.startswith("/") else ""}{path}'
+                        
+                        activity_handlers[activity_name] = list(set(activity_handlers.get(activity_name, []) + [uri]))
+
+    for activity, handlers in activity_handlers.items():
+        print(activity)
+        print('\n'.join(f'  {h}' for h in sorted(handlers)))
+
+
+if __name__ == '__main__':
+    main()
