93d5239ce3c20dae593c8b50c854acff771f45c0
inesmartins <39055313+inesmartins@users.noreply.github.com>
2021-08-20 18:32:41 +0100

    everything works, except package install validation

--- deeplink_analyser.py
+++ deeplink_analyser.py
@@ -5,20 +5,20 @@ import helpers.get_schemes
 import helpers.setup
 import os
 
-def main(manifest, strings, package, apk, verify):
-    activity_handlers = helpers.get_schemes.get_schemes(strings, manifest)
-    if verify:
-        helpers.setup.check_device_configs(package, apk)
-    for activity, handlers in activity_handlers.items():
-        print("\n" + activity + "\n")
-        for deeplink in sorted(handlers):
-            if "http" in deeplink:
-                print(deeplink)
-                os.system('adb shell am start -a android.intent.action.VIEW -d ' +  deeplink + ' ' + package)
-    else:
+def main(strings_file, manifest_file, package, apk, verify):
+    activity_handlers = helpers.get_schemes.get_schemes(strings_file, manifest_file)
+    if not verify:
         for activity, handlers in activity_handlers.items():
             print(activity)
             print('\n'.join(f'  {h}' for h in sorted(handlers)))
+    else:
+        helpers.setup.check_device_configs(package, apk)
+        for activity, handlers in activity_handlers.items():
+            print("\n" + activity + "\n")
+            for deeplink in sorted(handlers):
+                if "http" in deeplink:
+                    print(deeplink)
+                    os.system('adb shell am start -a android.intent.action.VIEW -d ' +  deeplink + ' ' + package)
 
 if __name__ == '__main__':
     args = helpers.setup.get_parsed_args()
@@ -29,11 +29,13 @@ if __name__ == '__main__':
         else:
             helpers.setup.decompile_apk(args.apk)
             apk_filename = os.path.basename(args.apk).split('.apk')[0]
-            manifest_file = open(apk_filename + "/AndroidManifest.xml")
-            strings_file = open(apk_filename + "/res/values/strings.xml")
-            main(manifest_file, strings_file, args.package, args.apk, args.verify)
+            strings_file_path = open(apk_filename + "/res/values/strings.xml")
+            manifest_file_path = open(apk_filename + "/AndroidManifest.xml")
+            main(strings_file_path, manifest_file_path, args.package, args.apk, args.verify)
             if args.clear:
                 print("Clearing decompiled directory")
                 os.system("rm -rf " + dir)
     else:
-        main(open(args.manifest), open(args.strings), args.package, args.apk, args.verify)
+        strings_file_path = open(args.strings)
+        manifest_file_path = open(args.manifest)
+        main(strings_file_path, manifest_file_path, args.package, args.apk, args.verify)
