ed594e18cdd2dcb27ab84acd45403543d6c1a1a6
inesmartins <39055313+inesmartins@users.noreply.github.com>
2021-08-22 20:52:10 +0100

    adds dal to console

--- helpers/app_links.py
+++ helpers/app_links.py
@@ -20,13 +20,18 @@ def sha256_is_in_dal(url, sha256):
     dal = subprocess.Popen(
         'curl ' + url + DEFAULT_DAL_FILE + ' -s', shell=True, stdout=subprocess.PIPE
     ).stdout.read().decode()
-    dal_json = json.loads(dal)
-    for entry in dal_json:
-        if "target" in entry:
-            if "sha256_cert_fingerprints" in entry["target"]:
-                if sha256 in entry["target"]["sha256_cert_fingerprints"]:
-                    return True
+    print(dal)
+    try:
+        dal_json = json.loads(dal)
+        for entry in dal_json:
+            if "target" in entry:
+                if "sha256_cert_fingerprints" in entry["target"]:
+                    if sha256 in entry["target"]["sha256_cert_fingerprints"]:
+                        return True
+    except:
+        return False
     return False
+
 
 def check_dal(url, sha256):
     helpers.console.write_to_console(
